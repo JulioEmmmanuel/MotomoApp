428d3c2bb0617dace0f016b7a21e45de8740c42c
julio2505 <juliomeza2510@outlook.com>
2023-05-21 04:42:38 -0600

    Added cart summary screen

---
+++ app/src/main/java/com/example/motomoapp/CartItem.kt
@@ -0,0 +1,10 @@
+package com.example.motomoapp
+
+data class CartItem (
+    val id:Int,
+    val name: String,
+    val precio: String,
+    val subtotal: String,
+    val idImagen: Int,
+    val cantidad: Int
+)
