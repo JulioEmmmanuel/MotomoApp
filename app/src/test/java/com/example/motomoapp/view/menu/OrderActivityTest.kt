7da1335d6d07994b3352d2c45dd6bbcb99794b9f
IrvinBSu <104798494+IrvinBSu@users.noreply.github.com>
2023-07-30 22:15:38 -0600

    Fixex Payment Activity

---
+++ app/src/test/java/com/example/motomoapp/view/menu/OrderActivityTest.kt
@@ -0,0 +1,23 @@
+package com.example.motomoapp.view.menu
+
+import androidx.activity.viewModels
+import androidx.lifecycle.MutableLiveData
+import com.example.motomoapp.databinding.ActivityOrderBinding
+import com.example.motomoapp.viewmodels.MenuViewModel
+import com.example.motomoapp.viewmodels.PedidoViewModel
+import org.junit.Test
+
+
+
+
+internal class OrderActivityTest{
+
+    private lateinit var binding: ActivityOrderBinding
+    //private val menuViewModel: MenuViewModel by viewModels()
+
+    @Test
+    fun getMainDishes_empty_returnsZero() {
+        //val mainDishes = menuViewModel.mainDishes.observe(this)
+        //val result =
+    }
+}
